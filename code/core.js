ec57827616659e2ed9f5ce48309afa77595a9589
s-laugh <shaunlaughland@gmail.com>
2019-02-08 10:53:30 -0500

    Adding defaults for layout completes #1 works on #3

---
+++ code/core.js
@@ -0,0 +1,37 @@
+var title = new Vue({
+  el: 'title',
+  data: {
+    title: 'App Hub'
+  }
+});
+
+var appDetails = new Vue({
+  el: '#appDetails',
+  data: {
+    name: 'App Hub',
+    description: 'The App Hub, otherwise known as the Application Dashborad Hub. The App Hub is a dashboard to show application pipeline status for multiple solutions in ESDC.',
+    build: 'Built by the Developer Experince team.'
+  }
+});
+
+var coreDataTable = new Vue({
+  el: '#coreDataTable',
+  data: {
+    caption: 'Code Coverage Status',
+    slnHeader: 'Solution',
+    coverageHeader: 'Coverage',
+    dateHeader: 'Date Updated',
+    solutions: [
+      {
+        name: 'app1',
+        value: '0',
+        date: 'n/a'
+      },
+      {
+        name: 'App 2',
+        value: '80',
+        date: '2019/02/04 12:15'
+      },
+    ]
+  }
+});
