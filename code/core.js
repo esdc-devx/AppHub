f1b83e916003b9db01961d46fbf0d0df27837cb3
Shaun Laughland <shaunlaughland@gmail.com>
2019-02-20 12:14:37 -0500

    Add Test framework (#11)
    * adding test

--- code/core.js
+++ code/core.js
@@ -1,45 +1,33 @@
-  var developer = "Developer Experience"
-  var appName = "App Hub";
-  var appNameFull = "Application Dashboard Hub";
-  var appDescription = 'The ' + appName + ', otherwise known as the ' + appNameFull + '. The ' + appName + ' is a dashboard to show application pipeline status for multiple solutions in ESDC.';
-  var ccData = [
-    {
-      name: 'app1',
-      cdCoverage: '0',
-      date: 'n/a'
-    },
-    {
-      name: 'App 2',
-      cdCoverage: '80',
-      date: '2019/02/04 12:15'
-    },
-  ];
+var developer = "Developer Experience";
+var appName = "App Hub";
+var appNameFull = "Application Dashboard Hub";
+var appDescription = 'The ' + appName + ', otherwise known as the ' + appNameFull + '. The ' + appName + ' is a dashboard to show application pipeline status for multiple solutions in ESDC.';
 
-  var head = new Vue({
-    el: 'head',
-    data: {
-      title: appName + ' - ' + developer,
-      description: appDescription,
-      author: 'GoC - ' + developer
-    }
-  });
+var head = new Vue({
+  el: 'head',
+  data: {
+    title: appName + ' - ' + developer,
+    description: appDescription,
+    author: 'GoC - ' + developer
+  }
+});
 
-  var appDetails = new Vue({
-    el: '#appDetails',
-    data: {
-      name: appName,
-      description: appDescription,
-      build: 'Built by the ' + developer + ' team.'
-    }
-  });
+var appDetails = new Vue({
+  el: '#appDetails',
+  data: {
+    name: appName,
+    description: appDescription,
+    build: 'Built by the ' + developer + ' team.'
+  }
+});
 
-  var coreDataTable = new Vue({
-    el: '#coreDataTable',
-    data: {
-      caption: 'Code Coverage Status',
-      slnHeader: 'Solution',
-      coverageHeader: 'Coverage',
-      dateHeader: 'Date Updated',
-      solutions: ccData
-    }
-  });
+var coreDataTable = new Vue({
+  el: '#coreDataTable',
+  data: {
+    caption: 'Code Coverage Status',
+    slnHeader: 'Solution',
+    coverageHeader: 'Coverage',
+    dateHeader: 'Date Updated',
+    solutions: GetCCData()
+  }
+});
