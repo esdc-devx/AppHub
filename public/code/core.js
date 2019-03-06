c00119e8a942951bb558c93bbf75372c427d6fd6
Shaun Laughland <shaunlaughland@gmail.com>
2019-03-06 14:01:49 -0500

    Vuecli (#15)
    * Using the Vue CLI to do all the setup
    * renamed component
    * Adding Table to view
    * cleaning up dashboard code

---
+++ public/code/core.js
@@ -0,0 +1,33 @@
+var developer = "Developer Experience";
+var appName = "App Hub";
+var appNameFull = "Application Dashboard Hub";
+var appDescription = 'The ' + appName + ', otherwise known as the ' + appNameFull + '. The ' + appName + ' is a dashboard to show application pipeline status for multiple solutions in ESDC.';
+
+var head = new Vue({
+  el: 'head',
+  data: {
+    title: appName + ' - ' + developer,
+    description: appDescription,
+    author: 'GoC - ' + developer
+  }
+});
+
+var appDetails = new Vue({
+  el: '#appDetails',
+  data: {
+    name: appName,
+    description: appDescription,
+    build: 'Built by the ' + developer + ' team.'
+  }
+});
+
+var coreDataTable = new Vue({
+  el: '#coreDataTable',
+  data: {
+    caption: 'Code Coverage Status',
+    slnHeader: 'Solution',
+    coverageHeader: 'Coverage',
+    dateHeader: 'Date Updated',
+    solutions: GetCCData()
+  }
+});
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div id="appDetails">
+      <h1>{{ name }}</h1>
+      <p>{{ description }} <em>{{ build }}</em></p>
+    </div>
+    <DashBoardTable />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import DashBoardTable from "./components/DashBoardTable.vue";
+
+@Component({
+  components: {
+    DashBoardTable
+  }
+})
+export default class App extends Vue {
+  @Prop() private name = "App Hub";
+  @Prop() private description = 'The ' + this.name + ', otherwise known as the Application Dashboard Hub. The ' + this.name + ' is a dashboard to show application pipeline status for multiple solutions in ESDC.';
+  @Prop() private build = 'Built by the Developer Experience team.';
+}
+</script>
+
+<!-- Shcould be taken from the WET teamplate -->
+<style></style>
