041546a47232e35259da8bcffb87466e42247016
s-laugh <shaunlaughland@gmail.com>
2019-03-15 15:06:10 -0400

    injecting the apollo client not working for the test

--- app-hub-gui/src/data.ts
+++ app-hub-gui/src/data.ts
@@ -1,7 +1,7 @@
 import gql from "graphql-tag";
 import ApolloClient from "apollo-boost";
-import { makeExecutableSchema, addMockFunctionsToSchema } from "graphql-tools";
-import { graphql } from "graphql";
+// import { makeExecutableSchema, addMockFunctionsToSchema } from "graphql-tools";
+// import { graphql } from "graphql";
 
 interface CodeCoverageData {
   name: string;
@@ -11,29 +11,8 @@ interface CodeCoverageData {
 }
 
 export class CCData {
-  constructor() {}
-
-  async GetCCData(): Promise<CodeCoverageData[]> {
-    //copied from AppHubApi
-    //TODO sync up schema between api and gui
-    const typeDefs = gql`
-      # Comments in GraphQL are defined with the hash (#) symbol.
-      scalar Date
-
-      type Project {
-        name: String
-        coverage: Int
-        lastUpdated: Date
-      }
-
-      # The "Query" type is the root of all GraphQL queries.
-      # (A "Mutation" type will be covered later on.)
-      type Query {
-        projects: [Project]
-      }
-    `;
-
-    const query = gql`
+  public readonly client: ApolloClient<any>;
+  public readonly query = gql`
       {
         projects {
           name
@@ -43,19 +22,53 @@ export class CCData {
       }
     `;
 
-    const schema = makeExecutableSchema({ typeDefs: typeDefs });
-    addMockFunctionsToSchema({ schema });
+  //constructor()
+  constructor(client?: ApolloClient<any>) {
+    if (client) {
+      this.client = client;
+    } else {
+      this.client = new ApolloClient({
+        //TODO get from ENV Variable
+        uri: "http://localhost:4000"
+      });
+    }
+  }
 
-    graphql(schema, query).then(result => console.log("Got result", result));
+  async GetCCData(): Promise<CodeCoverageData[]> {
+    
 
-    const client = new ApolloClient({
-      //TODO get from ENV Variable
-      uri: "http://localhost:4000"
-    });
+    //copied from AppHubApi
+    //TODO sync up schema between api and gui
+    // const typeDefs = gql`
+    //   # Comments in GraphQL are defined with the hash (#) symbol.
+    //   scalar Date
+
+    //   type Project {
+    //     name: String
+    //     coverage: Int
+    //     lastUpdated: Date
+    //   }
+
+    //   # The "Query" type is the root of all GraphQL queries.
+    //   # (A "Mutation" type will be covered later on.)
+    //   type Query {
+    //     projects: [Project]
+    //   }
+    // `;
+
+    // const schema = makeExecutableSchema({ typeDefs: typeDefs });
+    // addMockFunctionsToSchema({ schema });
+
+    // graphql(schema, query).then(result => console.log("Got result", result));
+
+    // const client = new ApolloClient({
+    //   //TODO get from ENV Variable
+    //   uri: "http://localhost:4000"
+    // });
 
     try {
-      let result = await client.query({
-        query: query
+      let result = await this.client.query({
+        query: this.query
       });
       console.log(result);
       if (result.data.projects.length > 0) {
--- app-hub-gui/tests/unit/data.spec.ts
+++ app-hub-gui/tests/unit/data.spec.ts
@@ -1,46 +1,50 @@
-import { CCData } from "../../src/data"
+import { CCData } from "../../src/data";
+import ApolloClient from "apollo-boost";
 
 describe(CCData.name, () => {
+  const customFetcher = jest.fn(() =>
+    Promise.resolve({
+      text: () => Promise.resolve('{"data": {"foo": "bar" }}'),
+    })
+  );
 
-    const data = new CCData();
+  const client = new ApolloClient({
+    fetch : customFetcher
+  });
 
-    describe(data.GetCCAlert.name, () => { 
-        it("returns 'warning' with 10 provided", () => {
-            expect(data.GetCCAlert(10)).toBe('warning');
-        });
+  const data = new CCData(client);
 
-        it('returns "danger" with null provided', () => {
-            expect(data.GetCCAlert(undefined)).toBe('danger');
-        });
-        it('returns "danger" with 0 provided', () => {
-            expect(data.GetCCAlert(0)).toBe('danger');
-        });
-        it('returns "success" with 50 provided', () => {
-            expect(data.GetCCAlert(50)).toBe('success');
-        });
-    }) ;
-    
-    describe(data.GetCCData.name, () => {
-        it('returns 3 items', (done) => {
-            return data.GetCCData().then(result => { 
-                expect(result).toHaveLength(3);
-            });
-        });
-    }); 
-    
-    describe(data.GetCCDataWithAlert.name, () => { 
-        it('should properly map test data', () => {
-           
-            return data.GetCCDataWithAlert().then(result => { 
-
-                expect(result[0].status).toBe('danger');
-                expect(result[1].status).toBe('success');
-                expect(result[2].status).toBe('warning');
-
-            });
-        });
-    
+  describe(data.GetCCAlert.name, () => {
+    it("returns 'warning' with 10 provided", () => {
+      expect(data.GetCCAlert(10)).toBe("warning");
     });
-    
-});
 
+    it('returns "danger" with null provided', () => {
+      expect(data.GetCCAlert(undefined)).toBe("danger");
+    });
+    it('returns "danger" with 0 provided', () => {
+      expect(data.GetCCAlert(0)).toBe("danger");
+    });
+    it('returns "success" with 50 provided', () => {
+      expect(data.GetCCAlert(50)).toBe("success");
+    });
+  });
+
+  describe(data.GetCCData.name, () => {
+    it("returns 1 items", done => {
+      return data.GetCCData().then(result => {
+        expect(result).toHaveLength(3);
+      });
+    });
+  });
+
+  describe(data.GetCCDataWithAlert.name, () => {
+    it("should properly map test data", () => {
+      return data.GetCCDataWithAlert().then(result => {
+        expect(result[0].status).toBe("danger");
+        expect(result[1].status).toBe("success");
+        expect(result[2].status).toBe("warning");
+      });
+    });
+  });
+});
